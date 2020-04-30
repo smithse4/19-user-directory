ca3ae009eb48e98cbbe28178be6279d0a223cdc4
Stephanie smith <smithse4@gmail.com>
2020-04-30 18:12:03 -0400

    added employees.json file and updated file structure

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron jumbotron-fluid">
+  <div className="container">
+    <h1 className="display-4">User Directory</h1>
+    <p className="lead">Sort the user directory below by name. Search the directory by email address.</p>
+  </div>
+</div>
+    );
+};
+
+export default Jumbotron;
